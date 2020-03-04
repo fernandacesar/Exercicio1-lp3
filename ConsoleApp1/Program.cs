1587372f9f1a016ee2c1b6d5b31a43fc9a3af0d8
fernandacesar <fernandacesar24555@gmail.com>
2020-03-04 11:30:01 -0300

    Criação do repositorio

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ferlipe_exercicio1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nomes =
+             {
+                "Fernanda Cesar",
+                "Fellipe"
+            };
+            foreach(String nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+            Console.ReadKey();
+        }
+    }
+}
